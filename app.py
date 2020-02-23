ee391bb1f32e543450b5a10070c83061faf22edc
Johannes Mols <johannes.mols@googlemail.com>
2020-02-23 20:22:52 +0100

    Added database query support

--- app.py
+++ app.py
@@ -1,22 +1,60 @@
 import time
 import datetime
+import logging
 import config as cfg
+from db_access import *
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+
+logging.basicConfig(
+    filename='log/app.log',
+    filemode='w',
+    format="%(asctime)s - %(process)d - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.DEBUG
+)
 
 # Set up connection to the Bitcoin RPC client
 rpc_connection = AuthServiceProxy("http://%s:%s@%s" % (cfg.rpc['rpcuser'], cfg.rpc['rpcpassword'], cfg.rpc['endpoint']))
 
-first_iteration = True
-current_day = None
-active_block_hash = ""
 
-current_outputs = []
-current_freq_analysis = []
-current_size_analysis = []
-current_prot_analysis = []
+def execute():
+    first_iteration = True
+    current_day = None
+    active_block_hash = ""
 
-while True:
-    print()
+    current_outputs = []
+    current_freq_analysis = []
+    current_size_analysis = []
+    current_prot_analysis = []
+
+    while True:
+        try:
+            if first_iteration:
+                last_active_day = get_latest_active_day()
+                if last_active_day is not None:
+                    delete_data_after_date(last_active_day, 'FrequencyAnalysis')
+                    delete_data_after_date(last_active_day, 'SizeAnalysis')
+                    delete_data_after_date(last_active_day, 'ProtocolAnalysis')
+                first_iteration = False
+            else:
+                # todo: implement
+                print()
+        except JSONRPCException as e:
+            print(repr(e))
+            logging.critical(repr(e))
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except Exception as e:
+            print(repr(e))
+            logging.critical(repr(e))
+
+
+def main():
+    execute()
+
+
+if __name__ == '__main__':
+    main()
 
 # start_time = time.time()
 # best_block_hash = rpc_connection.getbestblockhash()
---
+++ db_access.py
@@ -0,0 +1,38 @@
+import pyodbc
+from app import cfg
+
+# Set up database connection
+db = pyodbc.connect(
+    'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + cfg.db['server'] + ';DATABASE=' + cfg.db['database'] +
+    ';UID=' + cfg.db['username'] + ";PWD=" + cfg.db['password'])
+cursor = db.cursor()
+
+
+def get_latest_active_day():
+    freq = __get_latest_analysis('FrequencyAnalysis')
+    size = __get_latest_analysis('SizeAnalysis')
+    prot = __get_latest_analysis('ProtocolAnalysis')
+
+    dates = []
+    if freq is not None:
+        dates.append(freq)
+    if size is not None:
+        dates.append(size)
+    if prot is not None:
+        dates.append(prot)
+
+    return min(dates) if len(dates) > 0 else None
+
+
+def __get_latest_analysis(table):
+    cursor.execute(
+        'SELECT * FROM dbo.' + table + ' WHERE DataDay = (SELECT MAX(DataDay) FROM dbo.' + table + ')')
+    row = cursor.fetchone()
+    if row:
+        return row.DataDay
+    else:
+        return None
+
+
+def delete_data_after_date(date, table):
+    cursor.execute('DELETE FROM dbo.' + table + ' WHERE DataDay >= \'' + date.strftime('%Y-%m-%d') + '\'')
